3539597bbbb82b8b3a362ea49bcf0cbfb810f313
crbn8d <wl.dlamini@gmail.com>
2021-02-12 13:53:52 +0200

    author update, author delete, book update, genre delete all function.

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -141,10 +141,67 @@ exports.author_delete_post = function(req, res, next) {
 
 // Display Author update form on GET.
 exports.author_update_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: Author update GET');
+        //    res.send('NOT IMPLEMENTED: Author update POST');
+		//author find by .id
+		Author.findById(req.params.id, {}, function (err,theauthor) { //find author by ID we can reference author by their object
+                if (err) { return next(err); }
+                   // Successful - redirect to book detail page.
+                   res.render('author_form', { title: 'Update Author', author: theauthor});
+				   //146: find author by their id, and then if found, ref author obj using "theauthor"
+				   //then want to render author form passing in the author obj
+                });
 };
 
 // Handle Author update on POST.
-exports.author_update_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: Author update POST');
-};
+//exports.author_update_post = function(req, res) {
+    //res.send('NOT IMPLEMENTED: Author update POST');
+//};
+
+// Handle author update on POST.
+exports.author_update_post = [
+
+    
+    // Validate and sanitise fields.
+    body('first_name').trim().isLength({ min: 1 }).escape().withMessage('First name must be specified.')
+        .isAlphanumeric().withMessage('First name has non-alphanumeric characters.'),
+    body('family_name').trim().isLength({ min: 1 }).escape().withMessage('Family name must be specified.')
+        .isAlphanumeric().withMessage('Family name has non-alphanumeric characters.'),
+    body('date_of_birth', 'Invalid date of birth').optional({ checkFalsy: true }).isISO8601().toDate(),
+    body('date_of_death', 'Invalid date of death').optional({ checkFalsy: true }).isISO8601().toDate(),
+   
+
+    // Process request after validation and sanitization.
+    (req, res, next) => {
+
+        // Extract the validation errors from a request.
+        const errors = validationResult(req);
+
+        // Create a Author object with escaped/trimmed data and old id.
+        var author = new Author(
+          { first_name: req.body.first_name,
+            family_name: req.body.family_name,
+            date_of_birth: req.body.date_of_birth,
+            date_of_death: req.body.date_of_death,
+            _id:req.params.id
+           });
+
+        if (!errors.isEmpty()) {
+            // There are errors. Render form again with sanitized values/error messages.
+			//redirect to the author update get 
+			//add console.log
+			
+			res.send('Form has errors');
+			
+           
+        }
+        else {
+            // Data from form is valid. Update the record.
+            Author.findByIdAndUpdate(req.params.id, author, {}, function (err,theauthor) {
+                if (err) { return next(err); }
+                   // Successful - redirect to author detail page.
+                   //res.redirect(theauthor.url); //might not work, something similar
+				   res.redirect(author.url);
+                });
+        }
+    }
+];
--- controllers/genreController.js
+++ controllers/genreController.js
@@ -98,13 +98,63 @@ exports.genre_create_post =  [
 ];
 
 // Display Genre delete form on GET.
-exports.genre_delete_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: Genre delete GET');
+//exports.genre_delete_get = function(req, res) {
+    //res.send('NOT IMPLEMENTED: Genre delete GET');
+//};
+
+// Display genre delete form on GET.
+exports.genre_delete_get = function(req, res, next) {
+
+    async.parallel({
+        genre: function(callback) {
+          Genre.findById(req.params.id).exec(callback)
+        },
+        genre_books: function(callback) {
+          Book.find({ 'genre': req.params.id }).exec(callback)
+        },
+    }, function(err, results) {
+        if (err) { return next(err); }
+        if (results.genre==null) { // No results.
+            res.redirect('/catalog/authors');
+        }
+        // Successful, so render.
+        res.render('genre_delete', { title: 'Delete Genre', genre: results.genre, genre_books: results.genre_books } );
+    });
+
 };
 
 // Handle Genre delete on POST.
-exports.genre_delete_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: Genre delete POST');
+//exports.genre_delete_post = function(req, res) {
+    //res.send('NOT IMPLEMENTED: Genre delete POST');
+//};
+
+// Handle genre delete on POST.
+exports.genre_delete_post = function(req, res, next) {
+
+    async.parallel({
+        genre: function(callback) {
+          Genre.findById(req.body.id).exec(callback)
+        },
+        genre_books: function(callback) {
+          Book.find({ 'genre': req.body.id }).exec(callback)
+        },
+    }, function(err, results) {
+        if (err) { return next(err); }
+        // Success
+        if (results.genre_books.length > 0) {
+            // genre has books. Render in same way as for GET route.
+            res.render('genre_delete', { title: 'Delete Genre', genre: results.genre, genre_books: results.genre_books } );
+            return;
+        }
+        else {
+            // Author has no books. Delete object and redirect to the list of authors.
+            Genre.findByIdAndRemove(req.body.id, function deleteGenre(err) {
+                if (err) { return next(err); }
+                // Success - go to author list
+                res.redirect('/catalog/genres')
+            })
+        }
+    });
 };
 
 // Display Genre update form on GET.
